72722a21bc8f9b09a232683282bdb01ca2cb8b49
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2026-04-06 15:11:48 -0400

    Change generateCmd.Flags() to use StringP for image flag
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>

--- internal/command/generate.go
+++ internal/command/generate.go
@@ -223,6 +223,6 @@ func init() {
 	generateCmd.Flags().StringP(generateCmdOutputFlag, "o", "manifests.yaml", "The output manifests file to write the manifests to")
 	generateCmd.Flags().String(generateCmdOverridesFileFlag, "", "An optional file of Score overrides to merge in")
 	generateCmd.Flags().StringArray(generateCmdOverridePropertyFlag, []string{}, "An optional set of path=key overrides to set or remove")
-	generateCmd.Flags().String(generateCmdImageFlag, "", "An optional container image to use for any container with image == '.'")
+	generateCmd.Flags().StringP(generateCmdImageFlag, "i", "An optional container image to use for any container with image == '.'")
 	rootCmd.AddCommand(generateCmd)
 }
